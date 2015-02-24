14ab08fcc436c43f649dc0f65a608b5705e09700
Niklas Schreiber <niklas@derschreiber.com>
2015-02-24 20:23:50 +0100

    change options_text for line-item

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -16,32 +16,29 @@ module Spree
     # end
 
     def options_text
-      str = Array.new
-      unless self.ad_hoc_option_values.empty? 
-
-        #TODO: group multi-select options (e.g. toppings) 
-        str << self.ad_hoc_option_values.each { |pov| 
-          "#{pov.option_value.option_type.presentation} = #{pov.option_value.presentation}"
-        }.join(',')
-      end # unless empty? 
+      str = []
+      if self.ad_hoc_option_values.present?
+        str << self.ad_hoc_option_values.each { |pov|
+          "#{pov.option_value.option_type.presentation}: #{pov.option_value.presentation}"
+        }.join(",")
+      end
 
       unless self.product_customizations.empty? 
         self.product_customizations.each do |customization| 
-          price_adjustment = (customization.price == 0) ? "" : " (#{Spree::Money.new(customization.price).to_s})"
-          str << "#{customization.product_customization_type.presentation}#{price_adjustment}"
+          #price_adjustment = (customization.price == 0) ? "" : " (#{Spree::Money.new(customization.price).to_s})"
+          #str << "#{customization.product_customization_type.presentation}#{price_adjustment}"
           customization.customized_product_options.each do |option| 
-            next if option.empty? 
-
+            next if option.empty?
             if option.customization_image? 
-              str << "#{option.customizable_product_option.presentation} = #{File.basename option.customization_image.url}"
+              str << "#{option.customizable_product_option.presentation}: #{File.basename option.customization_image.url}"
             else 
-              str << "#{option.customizable_product_option.presentation} = #{option.value}"
+              str << "#{option.customizable_product_option.presentation}: #{option.value.gsub('mass', 'maß').capitalize}"
             end 
           end # each option 
         end # each customization 
       end # unless empty? 
 
-      str.join('\n')
+      str.join("\n")
     end
 
     def cost_price
