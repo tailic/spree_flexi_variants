83897518f67e3ec8ab3cf543448261895260dc21
Niklas Schreiber <niklas@derschreiber.com>
2016-05-09 13:15:35 +0200

    new PDS layout

--- app/overrides/add_customizations_to_cart_form.rb
+++ app/overrides/add_customizations_to_cart_form.rb
@@ -1,4 +1,4 @@
     Deface::Override.new(:virtual_path => "spree/products/_cart_form",
                          :name => "converted_product_price_290656527",
-                         :insert_before => "[data-hook='product_price'], #product_price[data-hook]",
+                         :insert_before => ".add-to-cart",
                          :partial => "spree/products/customizations")
